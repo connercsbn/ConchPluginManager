62240d26094d38c55200e0d79718b6f71509a591
conner <connerluker@gmail.com>
2023-12-26 18:45:34 -0800

    biggish update

--- ConchPluginManager.cs
+++ ConchPluginManager.cs
@@ -28,7 +28,18 @@ public class ConchPluginManager : BasePlugin, IPluginConfig<ConchPluginManagerCo
         Config = config;
     }
 
-    [ConsoleCommand("css_update", "Update plugins")]
+    [ConsoleCommand("css_list", "List installed plugins")]
+    [CommandHelper(whoCanExecute: CommandUsage.CLIENT_AND_SERVER)]
+    [RequiresPermissions("@css/root")]
+    public void OnCommandList(CCSPlayerController? _, CommandInfo command)
+    {
+        foreach (var plugin in Config.PluginsInstalled)
+        {
+            command.ReplyToCommand(plugin.ToString());
+        }
+    }
+
+    [ConsoleCommand("css_update", "Update all")]
     [CommandHelper(whoCanExecute: CommandUsage.CLIENT_AND_SERVER)]
     [RequiresPermissions("@css/root")]
     public async void OnCommandUpdate(CCSPlayerController? _, CommandInfo command)
@@ -48,31 +59,29 @@ public class ConchPluginManager : BasePlugin, IPluginConfig<ConchPluginManagerCo
             return;
         }
         var newPlugin = new PluginInfo(downloadString); 
-        Config.PluginsInstalled.Add(newPlugin);
 
-        using var httpClient = new HttpClient();
-        httpClient.DefaultRequestHeaders.Add("User-Agent", "connercsbn/ConchPluginManager");
-        await CheckForUpdate(newPlugin, httpClient);
-        File.WriteAllText(configPath!, JsonSerializer.Serialize(Config, new JsonSerializerOptions { WriteIndented = true }));
+        if (!await CheckForUpdate(newPlugin, httpClient))
+        {
+            command.ReplyToCommand("");
+            return;
+        }
+        Config.PluginsInstalled.Add(newPlugin);
+        WriteConfig();
         Logger.LogInformation("loading plugin {plugin}", newPlugin.Directory);
         Server.NextFrame(() => Server.ExecuteCommand($"css_plugins load {newPlugin.Directory}"));
     }
 
-    [ConsoleCommand("css_remove", "Remove plugins")]
-    [CommandHelper(minArgs: 1, usage: "<Plugin>", whoCanExecute: CommandUsage.CLIENT_AND_SERVER)]
+    [ConsoleCommand("css_remove", "Remove a plugin")]
+    [CommandHelper(minArgs: 1, usage: "<Plugin download_string/directory>", whoCanExecute: CommandUsage.CLIENT_AND_SERVER)]
     [RequiresPermissions("@css/root")]
     public void OnCommandRemove(CCSPlayerController? _, CommandInfo command)
     {
         var arg = command.GetArg(1);
-        var pluginToRemove = Config.PluginsInstalled.Find((plugin) => plugin.Directory == arg);
+        var pluginToRemove = Config.PluginsInstalled.Find((plugin) => plugin.DownloadString == arg);
+        pluginToRemove ??= Config.PluginsInstalled.Find((plugin) => plugin.Directory == arg);
         if (pluginToRemove == null)
         { 
             Logger.LogInformation("Plugin {plugin} not found", arg);
-            Logger.LogInformation("available plugins: ");
-            foreach (var plugin in Config.PluginsInstalled)
-            {
-                Logger.LogInformation(plugin.Directory);
-            }
             return;
         }
         Logger.LogInformation("Removing plugin {plugin}", arg);
@@ -82,7 +91,9 @@ public class ConchPluginManager : BasePlugin, IPluginConfig<ConchPluginManagerCo
         {
             Logger.LogInformation("Deleting directory {dir}", pluginToRemovePath);
             Directory.Delete(pluginToRemovePath, true);
+            Logger.LogInformation("Plugin directory {dir} has been deleted. You may need to unload the plugin with css_plugins unload <plugin_name>", pluginToRemove.Directory);
             Config.PluginsInstalled.Remove(pluginToRemove);
+            WriteConfig();
         }
         else
         {
@@ -110,52 +121,77 @@ public class ConchPluginManager : BasePlugin, IPluginConfig<ConchPluginManagerCo
             if (!plugin.AutoUpdate) continue;
             await CheckForUpdate(plugin, httpClient); 
         }
+        WriteConfig();
     }
-    private async Task CheckForUpdate(PluginInfo plugin, HttpClient httpClient)
+    private void WriteConfig()
+    { 
+        if (configPath != null)
+        { 
+            File.WriteAllText(configPath!, JsonSerializer.Serialize(Config, new JsonSerializerOptions { WriteIndented = true }));
+        }
+    }
+    private async Task<bool> CheckForUpdate(PluginInfo plugin, HttpClient httpClient)
     {
+        var githubLink = $"https://api.github.com/repos/{plugin.DownloadString}/releases/latest";
         try
         {
             // for now, just download from latest release. TODO handle plugins that are released through the repository 
             Logger.LogInformation("querying https://api.github.com/repos/{plugin}/releases/latest", plugin.DownloadString);
-            HttpResponseMessage response = await httpClient.GetAsync($"https://api.github.com/repos/{plugin.DownloadString}/releases/latest");
+            HttpResponseMessage response = await httpClient.GetAsync(githubLink);
             try
             { 
                 var res = JsonSerializer.Deserialize<GithubApiResponse>(await response.Content.ReadAsStringAsync());
-                if (String.IsNullOrEmpty(res!.TagName)) return;
+                if (res == null || String.IsNullOrEmpty(res!.TagName))
+                { 
+                    Logger.LogInformation("Couldn't retrieve Github release information after querying {download_string}", plugin.DownloadString);
+                    return false;
+                }
                 if (res.TagName == plugin.TagName)
                 {
                     Logger.LogInformation("Plugin {plugin} up to date", plugin.DownloadString);
-                    return;
+                    return false;
                 }
-                Logger.LogInformation("Plugin {plugin} is not up to date! \"Downloading new release {tag_name} from {url}\"", plugin.DownloadString, res!.TagName, res!.Assets[0].BrowserDownloadUrl);
+                Logger.LogInformation("Plugin {plugin} is not up to date! Downloading new release {tag_name} from {url}", plugin.DownloadString, res!.TagName, res!.Assets[0].BrowserDownloadUrl);
                 var extractPath = await Download(plugin, res);
                 try
                 {
-                    plugin.Directory = Path.GetFileName(GetPluginDir(extractPath));
-                    Merge(extractPath);
-                    Logger.LogInformation("updating tag name from {previousTag} to {nextTag}", plugin.TagName, res!.TagName);
-                    plugin.TagName = res!.TagName;
-                    File.WriteAllText(configPath!, JsonSerializer.Serialize(Config, new JsonSerializerOptions { WriteIndented = true }));
+                    var pluginDir = GetPluginDir(extractPath);
+                    if (pluginDir == null)
+                    {
+                        Logger.LogInformation("Couldn't find plugin directory with matching dll. Aborting installation.");
+                        return false;
+                    }
+                    plugin.Directory = new DirectoryInfo(pluginDir).Name; 
+                    if (plugin.Directory == null)
+                    {
+                        Logger.LogInformation("TODO write this error message not sure why this would ever happen but probably will at some point, right?");
+                        return false;
+                    }
+                    Merge(extractPath, pluginDir);
+                    Logger.LogInformation("updating tag name from {previous_tag} to {next_tag}", plugin.TagName, res.TagName);
+                    plugin.TagName = res.TagName;
+                    return true;
                 }
                 catch (Exception ex)
                 {
-                    Logger.LogInformation(ex.Message);
+                    Logger.LogInformation("{error_message}", ex.Message);
                 }
             }
-            catch
+            catch (Exception ex)
             {
-                Logger.LogInformation(await response.Content.ReadAsStringAsync());
+                Logger.LogInformation("{error_message}", ex.Message);
+                Logger.LogInformation("{response_content}", await response.Content.ReadAsStringAsync());
             }
         }
         catch (Exception ex)
         {
-            Logger.LogInformation("An error occurred: {message}", ex.Message);
+            Logger.LogInformation("An error occurred while querying {github_link}:\n {message}", githubLink, ex.Message);
         }
+        return false;
     }
-    private void Merge(string extractPath)
+    private void Merge(string extractPath, string pluginDir)
     // sync to server files
     // any conflicting files will be overwritten, could this go wrong?
-    // TODO make sure the structure is counterstrikesharp/plugins/<PluginName>/<PluginName.dll> before merging
     {
         Logger.LogInformation("Merging");
         var csgo = new DirectoryInfo(ModulePath).Parent!.Parent!.Parent!.Parent!.Parent!;
@@ -190,19 +226,24 @@ public class ConchPluginManager : BasePlugin, IPluginConfig<ConchPluginManagerCo
                 }
             }
         }
-        var pluginDir = GetPluginDir(extractPath);
         Logger.LogInformation("{dir} ----> {plugins_dir}", pluginDir, plugins);
-        Move(pluginDir, Path.Join(plugins.FullName, new DirectoryInfo(pluginDir).Name));
+        Move(pluginDir, Path.Join(plugins.FullName, Path.GetFileName(pluginDir)));
     }
 
-    public string GetPluginDir(string extractPath)
-    { 
-        // there are no common directories, so we find the plugin and put it in plugins dir
-        var dlls = Directory.GetFiles(extractPath, "*.dll", SearchOption.AllDirectories);
-        Logger.LogInformation("dlls found: {dlls}", string.Join(", ", dlls)); 
-        // assuming all dlls are in the same directory...
-        var pluginDir = Path.GetDirectoryName(dlls[0])!;
-        return pluginDir;
+    public string? GetPluginDir(string extractPath)
+    {
+        // there are no common directories, so we find the plugin and put it in plugins dir 
+        foreach (var dir in new DirectoryInfo(extractPath).GetDirectories())
+        {
+            Logger.LogInformation("looking through {dir} for a matching .dll", dir.FullName);
+            var matchingDlls = Directory.GetFiles(dir.FullName, $"{dir.Name}.dll", SearchOption.TopDirectoryOnly);
+            Logger.LogInformation("Done...");
+            if (matchingDlls.Length == 1)
+            { 
+                return Path.GetDirectoryName(matchingDlls[0]);
+            }
+        }
+        return null;
     }
 
     static void Move(string sourceDirectory, string destinationDirectory)
@@ -227,15 +268,12 @@ public class ConchPluginManager : BasePlugin, IPluginConfig<ConchPluginManagerCo
             string tempPath = Path.Combine(destinationDirectory, subdir.Name);
             Move(subdir.FullName, tempPath);
         }
-        Directory.Delete(sourceDirectory);
+        Directory.Delete(sourceDirectory, true);
     } 
 
 
     public async Task<string> Download(PluginInfo plugin, GithubApiResponse ghRes)
     {
-        using HttpClient httpClient = new();
-        httpClient.DefaultRequestHeaders.Add("User-Agent", "connercsbn/ConchPluginManager");
-        httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "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");
         // for now, we problematically assume the download is in assets[0] and that it's a zip file 
         // TODO figure out a way to either automatically get the correct asset or allow the user to decide which one they want in the form of a chat menu thing
         HttpResponseMessage res = await httpClient.GetAsync(ghRes.Assets[0].BrowserDownloadUrl); 
@@ -265,7 +303,7 @@ public class ConchPluginManager : BasePlugin, IPluginConfig<ConchPluginManagerCo
         Logger.LogInformation("File downloaded successfully: {path}", filePath);
         var extractPath = Path.Combine(tempDirectory, Path.GetFileNameWithoutExtension(filePath)); 
         Logger.LogInformation("unzipping...");
-        ZipFile.ExtractToDirectory(filePath, extractPath);
+        ZipFile.ExtractToDirectory(filePath, extractPath, overwriteFiles: true);
         Logger.LogInformation("deleting...");
         File.Delete(filePath);
         Logger.LogInformation("Extracted file {path} to {dir}", filePath, extractPath);
@@ -283,6 +321,10 @@ public class PluginInfo
     public string? Directory { get; set; }
     public string? TagName { get; set; }
     public bool AutoUpdate { get; set; } = true;
+    public override string ToString()
+    {
+        return $"{DownloadString} is installed in plugins/{Directory} with TagName {TagName}";
+    }
 }
 
 public class ConchPluginManagerConfig : BasePluginConfig
